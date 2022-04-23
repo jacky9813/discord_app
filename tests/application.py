4c13e9716d3e14f5ccd65ba9d851cec07a47e138
JackyCCC <jacky9813@hotmail.com>
2022-04-23 15:49:23 +0800

    test: (application.py) remove unused imports

--- tests/application.py
+++ tests/application.py
@@ -2,20 +2,14 @@
 from os import environ as ENV  # For importing test secrets
 
 import discord_app.application
-import discord_app.command
-import discord_app.discord_types
-import discord_app.emoji
-import discord_app.user
-import discord_app.channel
-import discord_app.interaction
 
 
 app = discord_app.application.Application.from_basic_data(
-    ENV.get("DISCORD_APP_ID"),
-    ENV.get("DISCORD_APP_PUBLIC_KEY"),
-    ENV.get("DISCORD_APP_BOT_TOKEN")
+    ENV.get("DISCORD_APP_ID"),  # type: ignore[arg-type]
+    ENV.get("DISCORD_APP_PUBLIC_KEY"),  # type: ignore[arg-type]
+    ENV.get("DISCORD_APP_BOT_TOKEN")  # type: ignore[arg-type]
 )
 
 
 def test_application() -> None:
-    assert app.id == ENV.get("DISCORD_APP_ID")
+    assert app.id == ENV.get("DISCORD_APP_ID")  # type: ignore[attr-defined]
