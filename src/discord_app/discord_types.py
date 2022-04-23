9fd2cbeee1080e75d600ecd9f4e7e7c52e12a3f0
JackyCCC <jacky9813@hotmail.com>
2022-04-23 17:08:54 +0800

    feat: (presence.py) Activity related dataclasses

--- src/discord_app/discord_types.py
+++ src/discord_app/discord_types.py
@@ -403,6 +403,26 @@ class InviteTargetType(IntEnum):
 
 
 #############################################################
+#   Presence
+#############################################################
+
+
+class ActivityFlag(IntFlag):
+    """
+        Activity flags
+    """
+    INSTANCE = 1 << 0
+    JOIN = 1 << 1
+    SPECTATE = 1 << 2
+    JOIN_REQUEST = 1 << 3
+    SYNC = 1 << 4
+    PLAY = 1 << 5
+    PARTY_PRIVACY_FRIENDS = 1 << 6
+    PARTY_PRIVACY_VOICE_CHANNEL = 1 << 7
+    EMBEDDED = 1 << 8
+
+
+#############################################################
 #   User
 #############################################################
 
--- src/discord_app/presence.py
+++ src/discord_app/presence.py
@@ -1,6 +1,6 @@
 
 from dataclasses import dataclass
-from typing import Optional, Any
+from typing import Optional, List, Tuple
 
 from . import emoji as emoji_module
 from . import discord_types
@@ -13,23 +13,73 @@ class ActivityTimestamp(discord_types.DiscordDataClass):
     end: Optional[int] = None
 
 
+ActivityEmoji = emoji_module.Emoji  # Activity Emoji is essentially a subset of Emoji with the same required properties
+
+
+@dataclass
+class ActivityParty(discord_types.DiscordDataClass):
+    id: Optional[str] = None
+    size: Optional[Tuple[int, int]] = None  # current_size, max_size
+
+
+@dataclass
+class ActivityAsset(discord_types.DiscordDataClass):
+    large_image: Optional[str] = None
+    large_text: Optional[str] = None
+    small_image: Optional[str] = None
+    small_text: Optional[str] = None
+
+
+@dataclass
+class ActivitySecret(discord_types.DiscordDataClass):
+    join: Optional[str] = None
+    spectate: Optional[str] = None
+    match: Optional[str] = None
+
+
+@dataclass
+class ActivityButton(discord_types.DiscordDataClass):
+    label: str
+    url: str
+
+
 @dataclass
 class Activity(discord_types.DiscordDataClass):
     name: str
     type: discord_types.ActivityType
-    created_at: int
+    created_at: int  # Use unix timestamp here (in milliseconds)
     url: Optional[str] = None
     timestamps: Optional[ActivityTimestamp] = None
     application_id: Optional[discord_types.Snowflake] = None
     details: Optional[str] = None
     state: Optional[str] = None
-    emoji: Optional[emoji_module.Emoji] = None
-    party: Optional[Any] = None
-    assets: Optional[Any] = None
-    secrets: Optional[Any] = None
+    emoji: Optional[ActivityEmoji] = None
+    party: Optional[ActivityParty] = None
+    assets: Optional[ActivityAsset] = None
+    secrets: Optional[ActivitySecret] = None
     instance: Optional[bool] = None
-    flags: Optional[int] = None
-    buttons: Optional[Any] = None
+    flags: Optional[discord_types.ActivityFlag] = None
+    buttons: Optional[List[ActivityButton]] = None
+
+    def __post_init__(self) -> None:
+        self.type = discord_types.ActivityType(self.type)
+        if isinstance(self.timestamps, dict):
+            self.timestamps = ActivityTimestamp(**self.timestamps)  # type: ignore[unreachable]
+        if isinstance(self.emoji, dict):
+            self.emoji = ActivityEmoji(**self.emoji)  # type: ignore[unreachable]
+        if isinstance(self.party, dict):
+            self.party = ActivityParty(**self.party)  # type: ignore[unreachable]
+        if isinstance(self.assets, dict):
+            self.assets = ActivityAsset(**self.assets)  # type: ignore[unreachable]
+        if isinstance(self.secrets, dict):
+            self.secrets = ActivitySecret(**self.secrets)  # type: ignore[unreachable]
+        if isinstance(self.flags, int):
+            self.flags = discord_types.ActivityFlag(self.flags)
+        if isinstance(self.buttons, list):
+            self.buttons = [
+                ActivityButton(**b) if isinstance(b, dict) else b
+                for b in self.buttons
+            ]
 
 
 @dataclass
@@ -44,5 +94,16 @@ class PresenceUpdateEvent(discord_types.DiscordDataClass):
     user: user.User
     guild_id: discord_types.Snowflake
     status: str
-    activities: Activity
+    activities: List[Activity]
     client_status: ClientStatus
+
+    def __post_init__(self) -> None:
+        if isinstance(self.user, dict):
+            self.user = user.User(**self.user)  # type: ignore[unreachable]
+        if isinstance(self.activities, list):
+            self.activities = [
+                Activity(**a) if isinstance(a, dict) else a
+                for a in self.activities
+            ]
+        if isinstance(self.client_status, dict):
+            self.client_status = ClientStatus(**self.client_status)  # type: ignore[unreachable]
