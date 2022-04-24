b750fc384dbbb362277aaae6e66d852034d5e87d
JackyCCC <jacky9813@hotmail.com>
2022-04-24 11:43:31 +0800

    feat: (GuildScheduledEvent) __post_init__ implemented

--- src/discord_app/guild_scheduled_event.py
+++ src/discord_app/guild_scheduled_event.py
@@ -27,12 +27,28 @@ class GuildScheduledEvent(discord_types.DiscordDataClass):
     user_count: Optional[int] = None
     image: Optional[str] = None
 
-    # Required for STAGE_INSTANCE and VOICE
+    # Required for STAGE_INSTANCE and VOICE type
     channel_id: Optional[discord_types.Snowflake] = None
 
-    # Required for EXTERNAL
+    # Required for EXTERNAL type
     entity_metadata: Optional[GuildScheduledEventEntityMetadata] = None
     scheduled_end_time: Optional[str] = None
+
+    def __post_init__(self) -> None:
+        self.privacy_level = discord_types.GuildScheduledEventPrivacyLevel(self.privacy_level)
+        self.status = discord_types.GuildScheduledEventStatus(self.status)
+        self.entity_type = discord_types.GuildScheduledEventEntityType(self.entity_type)
+        if isinstance(self.creator, dict):
+            self.creator = user_module.User(**self.creator)  # type: ignore[unreachable]
+
+        if self.entity_type is discord_types.GuildScheduledEventEntityType.EXTERNAL:
+            if self.entity_metadata is None or self.scheduled_end_time is None:
+                raise AttributeError("entity_metadata and scheduled_end_time is required for EXTERNAL event type.")
+            if isinstance(self.entity_metadata, dict):
+                self.entity_metadata = GuildScheduledEventEntityMetadata(**self.entity_metadata)  # type: ignore[unreachable]
+        else:
+            if self.channel_id is None:
+                raise AttributeError(f"entity_metadata and scheduled_end_time is required for {self.entity_type.name} event type.")
 
 
 @dataclass
@@ -40,3 +56,9 @@ class GuildScheduledEventUser(discord_types.DiscordDataClass):
     guild_scheduled_event_id: discord_types.Snowflake
     user: 'user_module.User'
     member: Optional['guild_module.GuildMember']
+
+    def __post_init__(self) -> None:
+        if isinstance(self.user, dict):
+            self.user = user_module.User(**self.user)  # type: ignore[unreachable]
+        if isinstance(self.member, dict):
+            self.member = guild_module.GuildMember(**self.member)  # type: ignore[unreachable]
